33e3c6b53939d0cae88992ff00fd46c9fcc2f5f6
SegF4ult <xtreme984@gmail.com>
2013-01-22 20:42:43 +0100

    Completed gamepad functions, added some pin functions

--- src/pad_tester/gamepad.c
+++ src/pad_tester/gamepad.c
@@ -11,14 +11,33 @@ extern unsigned char PAD2;
 #define 1P_MODE
 #endif
 
-void InitGPads(void) {
-	
+void InitGPadIO(void) {
+	DIRA &= ~((1<<GP_D1)|(1<<GP_D2))		// Set GP_D1 and GP_D2 as INPUT
+	DIRA |= ((1<<GP_CLK)|(1<<GP_LATCH))		// Set GP_CLK and GP_LATCH as OUTPUT
+
+	pinOut(GP_LATCH,HIGH);
 }
 
 void readPads(void) {
-	
+	PAD1 = readButtons(GP_D1);
+	#ifdef 2P_MODE
+	PAD2 = readButtons(GP_D2);
+	#endif
 }
 
-void readButtons(int) {
-	
+uint8_t readButtons(int srcPin) {
+	int i = 0;
+	pinOut(GP_LATCH, LOW);
+	pinOut(GP_CLK, 	LOW);
+	uint8_t pad_vector = 0;
+	uint8_t input = 0;
+	for(i = PAD_BIT_LENGTH; i>0; i--) {
+		input = (INA & (1<<srcPin)) ? 0 : 1;
+		pad_vector |= (input<<(PAD_BIT_LENGTH-i));
+		pinSet(GP_CLK, HIGH);
+		waitcnt(500+CNT);
+		pinSet(GP_CLK, LOW);
+	}
+	pinSet(GP_LATCH, HIGH);
+	return pad_vector;
 }
--- src/pad_tester/pins.h
+++ src/pad_tester/pins.h
@@ -1,11 +1,13 @@
 #ifndef __GBL_PINS_H
 #define __GBL_PINS_H
 
+// Define GamePad pins
 #define GP_CLK		8
 #define GP_LATCH	9
 #define GP_D1		10
 #define GP_D2		11
 
+// Define SPI pins
 #define SPI_SCK		0
 #define SPI_MOSI	1
 #define SPI_MISO	2
@@ -13,6 +15,11 @@
 #define SPI_SRAM_CS	4
 #define SPI_VRAM_CS	5
 
-void pinSet(int,int);
+// PIN LOGIC LEVELS
+#define HIGH		1
+#define LOW			0
+
+void	pinSet(int,int);
+uint8_t	pinIn(int);
 
 #endif
